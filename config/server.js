e3e31bfd780ac93f387c8839467ff41e0b9cc549
unknown <adailsonacj@live.com>
2020-09-01 00:38:55 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cb23037ce522486cc920c170d49cb433'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '435a55b4-5c31-4599-a1ee-218d34a719dd'
+};
