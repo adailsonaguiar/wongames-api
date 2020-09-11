d5293c81f7ed4a2004121891caa011a94467a0dc
Adailson Aguiar <adailsoacj@live.com>
2020-09-11 00:59:17 -0300

    feat: colors of menu

--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -1,36 +1,19 @@
-import styled from "styled-components";
-import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
 import Logo from "../../../assets/images/logo-won.svg";
 
 const Wrapper = styled.div`
-  background-color: #007eff;
-  height: ${(props) => props.theme.main.sizes.leftMenu.height};
+  ${({ theme }) => css`
+    background-color: ${theme.main.colors.won.blue};
+    height: ${theme.main.sizes.leftMenu.height};
 
-  .projectName {
-    display: block;
-    height: ${(props) => props.theme.main.sizes.leftMenu.height};
-    background-image: url(${Logo});
-    background-position: center center;
-  }
+    .projectName {
+      display: block;
+      height: ${theme.main.sizes.leftMenu.height};
+      background-image: url(${Logo});
+      background-position: center center;
+    }
+  `}
 `;
-
-Wrapper.defaultProps = {
-  theme: {
-    main: {
-      colors: {
-        leftMenu: {},
-      },
-      sizes: {
-        header: {},
-        leftMenu: {},
-      },
-    },
-  },
-};
-
-Wrapper.propTypes = {
-  theme: PropTypes.object,
-};
 
 export default Wrapper;
